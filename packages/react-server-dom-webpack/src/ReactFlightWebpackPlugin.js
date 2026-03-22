AbanoubGhadban/react

commit 701ac2e57290f913502c6012c15e815f412f84b7

[Flight][Float] Preinitialize module imports during SSR (#27314)
Currently when we SSR a Flight response we do not emit any resources for
module imports. This means that when the client hydrates it won't have
already loaded the necessary scripts to satisfy the Imports defined in
the Flight payload which will lead to a delay in hydration completing.

This change updates `react-server-dom-webpack` and
`react-server-dom-esm` to emit async script tags in the head when we
encounter a modules in the flight response.

To support this we need some additional server configuration. We need to
know the path prefix for chunk loading and whether the chunks will load
with CORS or not (and if so with what configuration).

packages/react-server-dom-webpack/src/ReactFlightWebpackPlugin.js
@@ -7,6 +7,8 @@
  * @flow
  */
 
+import type {ImportManifestEntry} from './shared/ReactFlightImportMetadata';
+
 import {join} from 'path';
 import {pathToFileURL} from 'url';
 import asyncLib from 'neo-async';
@@ -221,21 +223,65 @@ export default class ReactFlightWebpackPlugin {
             return;
           }
 
+          const configuredCrossOriginLoading =
+            compilation.outputOptions.crossOriginLoading;
+          const crossOriginMode =
+            typeof configuredCrossOriginLoading === 'string'
+              ? configuredCrossOriginLoading === 'use-credentials'
+                ? configuredCrossOriginLoading
+                : 'anonymous'
+              : null;
+
           const resolvedClientFiles = new Set(
             (resolvedClientReferences || []).map(ref => ref.request),
           );
 
           const clientManifest: {
-            [string]: {chunks: $FlowFixMe, id: string, name: string},
+            [string]: ImportManifestEntry,
           } = {};
-          const ssrManifest: {
+          type SSRModuleMap = {
             [string]: {
               [string]: {specifier: string, name: string},
             },
-          } = {};
+          };
+          const moduleMap: SSRModuleMap = {};
+          const ssrBundleConfig: {
+            moduleLoading: {
+              prefix: string,
+              crossOrigin: string | null,
+            },
+            moduleMap: SSRModuleMap,
+          } = {
+            moduleLoading: {
+              prefix: compilation.outputOptions.publicPath || '',
+              crossOrigin: crossOriginMode,
+            },
+            moduleMap,
+          };
+
+          // We figure out which files are always loaded by any initial chunk (entrypoint).
+          // We use this to filter out chunks that Flight will never need to load
+          const emptySet: Set<string> = new Set();
+          const runtimeChunkFiles: Set<string> = emptySet;
+          compilation.entrypoints.forEach(entrypoint => {
+            const runtimeChunk = entrypoint.getRuntimeChunk();
+            if (runtimeChunk) {
+              runtimeChunk.files.forEach(runtimeFile => {
+                runtimeChunkFiles.add(runtimeFile);
+              });
+            }
+          });
+
           compilation.chunkGroups.forEach(function (chunkGroup) {
-            const chunkIds = chunkGroup.chunks.map(function (c) {
-              return c.id;
+            const chunks: Array<string> = [];
+            chunkGroup.chunks.forEach(function (c) {
+              // eslint-disable-next-line no-for-of-loops/no-for-of-loops
+              for (const file of c.files) {
+                if (!file.endsWith('.js')) return;
+                if (file.endsWith('.hot-update.js')) return;
+                chunks.push(c.id, file);
+                break;
+              }
             });
 
             // $FlowFixMe[missing-local-annot]
@@ -256,7 +302,7 @@ export default class ReactFlightWebpackPlugin {
 
                 clientManifest[href] = {
                   id,
-                  chunks: chunkIds,
+                  chunks,
                   name: '*',
                 };
                 ssrExports['*'] = {
@@ -272,7 +318,7 @@ export default class ReactFlightWebpackPlugin {
                 /*
                 clientManifest[href + '#'] = {
                   id,
-                  chunks: chunkIds,
+                  chunks,
                   name: '',
                 };
                 ssrExports[''] = {
@@ -288,7 +334,7 @@ export default class ReactFlightWebpackPlugin {
                   moduleProvidedExports.forEach(function (name) {
                     clientManifest[href + '#' + name] = {
                       id,
-                      chunks: chunkIds,
+                      chunks,
                       name: name,
                     };
                     ssrExports[name] = {
@@ -299,7 +345,7 @@ export default class ReactFlightWebpackPlugin {
                 }
                 */
 
-                ssrManifest[id] = ssrExports;
+                moduleMap[id] = ssrExports;
               }
             }
 
@@ -326,7 +372,7 @@ export default class ReactFlightWebpackPlugin {
             _this.clientManifestFilename,
             new sources.RawSource(clientOutput, false),
           );
-          const ssrOutput = JSON.stringify(ssrManifest, null, 2);
+          const ssrOutput = JSON.stringify(ssrBundleConfig, null, 2);
           compilation.emitAsset(
             _this.ssrManifestFilename,
             new sources.RawSource(ssrOutput, false),

fixtures/flight-esm/.nvmrc
@@ -1,0 +1,1 @@
+v18

fixtures/flight-esm/server/global.js
@@ -10,6 +10,7 @@ const compress = require('compression');
 const chalk = require('chalk');
 const express = require('express');
 const http = require('http');
+const React = require('react');
 
 const {renderToPipeableStream} = require('react-dom/server');
 const {createFromNodeStream} = require('react-server-dom-esm/client');
@@ -62,23 +63,39 @@ app.all('/', async function (req, res, next) {
   if (req.accepts('text/html')) {
     try {
       const rscResponse = await promiseForData;
-
       const moduleBaseURL = '/src';
 
       // For HTML, we're a "client" emulator that runs the client code,
       // so we start by consuming the RSC payload. This needs the local file path
       // to load the source files from as well as the URL path for preloads.
-      const root = await createFromNodeStream(
-        rscResponse,
-        moduleBasePath,
-        moduleBaseURL
-      );
+
+      let root;
+      let Root = () => {
+        if (root) {
+          return React.use(root);
+        }
+
+        return React.use(
+          (root = createFromNodeStream(
+            rscResponse,
+            moduleBasePath,
+            moduleBaseURL
+          ))
+        );
+      };
       // Render it into HTML by resolving the client components
       res.set('Content-type', 'text/html');
-      const {pipe} = renderToPipeableStream(root, {
-        // TODO: bootstrapModules inserts a preload before the importmap which causes
-        // the import map to be invalid. We need to fix that in Float somehow.
-        // bootstrapModules: ['/src/index.js'],
+      const {pipe} = renderToPipeableStream(React.createElement(Root), {
+        importMap: {
+          imports: {
+            react: 'https://esm.sh/react@experimental?pin=v124&dev',
+            'react-dom': 'https://esm.sh/react-dom@experimental?pin=v124&dev',
+            'react-dom/': 'https://esm.sh/react-dom@experimental&pin=v124&dev/',
+            'react-server-dom-esm/client':
+              '/node_modules/react-server-dom-esm/esm/react-server-dom-esm-client.browser.development.js',
+          },
+        },
+        bootstrapModules: ['/src/index.js'],
       });
       pipe(res);
     } catch (e) {
@@ -89,6 +106,7 @@ app.all('/', async function (req, res, next) {
   } else {
     try {
       const rscResponse = await promiseForData;
+
       // For other request, we pass-through the RSC payload.
       res.set('Content-type', 'text/x-component');
       rscResponse.on('data', data => {

fixtures/flight-esm/src/App.js
@@ -9,16 +9,6 @@ import {getServerState} from './ServerState.js';
 
 const h = React.createElement;
 
-const importMap = {
-  imports: {
-    react: 'https://esm.sh/react@experimental?pin=v124&dev',
-    'react-dom': 'https://esm.sh/react-dom@experimental?pin=v124&dev',
-    'react-dom/': 'https://esm.sh/react-dom@experimental&pin=v124&dev/',
-    'react-server-dom-esm/client':
-      '/node_modules/react-server-dom-esm/esm/react-server-dom-esm-client.browser.development.js',
-  },
-};
-
 export default async function App() {
   const res = await fetch('http://localhost:3001/todos');
   const todos = await res.json();
@@ -42,12 +32,6 @@ export default async function App() {
         rel: 'stylesheet',
         href: '/src/style.css',
         precedence: 'default',
-      }),
-      h('script', {
-        type: 'importmap',
-        dangerouslySetInnerHTML: {
-          __html: JSON.stringify(importMap),
-        },
       })
     ),
     h(
@@ -84,9 +68,7 @@ export default async function App() {
             'Like'
           )
         )
-      ),
-      // TODO: Move this to bootstrapModules.
-      h('script', {type: 'module', src: '/src/index.js'})
+      )
     )
   );
 }

fixtures/flight/.nvmrc
@@ -1,0 +1,1 @@
+v18

fixtures/flight/server/global.js
@@ -33,6 +33,7 @@ const compress = require('compression');
 const chalk = require('chalk');
 const express = require('express');
 const http = require('http');
+const React = require('react');
 
 const {renderToPipeableStream} = require('react-dom/server');
 const {createFromNodeStream} = require('react-server-dom-webpack/client');
@@ -62,6 +63,11 @@ if (process.env.NODE_ENV === 'development') {
     webpackMiddleware(compiler, {
       publicPath: paths.publicUrlOrPath.slice(0, -1),
       serverSideRender: true,
+      headers: () => {
+        return {
+          'Cache-Control': 'no-store, must-revalidate',
+        };
+      },
     })
   );
   app.use(webpackHotMiddleware(compiler));
@@ -121,12 +127,13 @@ app.all('/', async function (req, res, next) {
         buildPath = path.join(__dirname, '../build/');
       }
       // Read the module map from the virtual file system.
-      const moduleMap = JSON.parse(
+      const ssrManifest = JSON.parse(
         await virtualFs.readFile(
           path.join(buildPath, 'react-ssr-manifest.json'),
           'utf8'
         )
       );
+
       // Read the entrypoints containing the initial JS to bootstrap everything.
       // For other pages, the chunks in the RSC payload are enough.
       const mainJSChunks = JSON.parse(
@@ -138,15 +145,35 @@ app.all('/', async function (req, res, next) {
       // For HTML, we're a "client" emulator that runs the client code,
       // so we start by consuming the RSC payload. This needs a module
       // map that reverse engineers the client-side path to the SSR path.
-      const {root, formState} = await createFromNodeStream(
-        rscResponse,
-        moduleMap
-      );
+
+      // This is a bad hack to set the form state after SSR has started. It works
+      // because we block the root component until we have the form state and
+      // any form that reads it necessarily will come later. It also only works
+      // because the formstate type is an object which may change in the future
+      const lazyFormState = [];
+
+      let cachedResult = null;
+      async function getRootAndFormState() {
+        const {root, formState} = await createFromNodeStream(
+          rscResponse,
+          ssrManifest
+        );
+        // We shouldn't be assuming formState is an object type but at the moment
+        // we have no way of setting the form state from within the render
+        Object.assign(lazyFormState, formState);
+        return root;
+      }
+      let Root = () => {
+        if (!cachedResult) {
+          cachedResult = getRootAndFormState();
+        }
+        return React.use(cachedResult);
+      };
       // Render it into HTML by resolving the client components
       res.set('Content-type', 'text/html');
-      const {pipe} = renderToPipeableStream(root, {
+      const {pipe} = renderToPipeableStream(React.createElement(Root), {
         bootstrapScripts: mainJSChunks,
-        experimental_formState: formState,
+        experimental_formState: lazyFormState,
       });
       pipe(res);
     } catch (e) {

packages/react-client/src/ReactFlightClient.js
@@ -13,8 +13,9 @@ import type {LazyComponent} from 'react/src/ReactLazy';
 import type {
   ClientReference,
   ClientReferenceMetadata,
-  SSRManifest,
+  SSRModuleMap,
   StringDecoder,
+  ModuleLoading,
 } from './ReactFlightClientConfig';
 
 import type {
@@ -36,6 +37,7 @@ import {
   readPartialStringChunk,
   readFinalStringChunk,
   createStringDecoder,
+  prepareDestinationForModule,
 } from './ReactFlightClientConfig';
 
 import {registerServerReference} from './ReactFlightReplyClient';
@@ -178,8 +180,10 @@ Chunk.prototype.then = function <T>(
 };
 
 export type Response = {
-  _bundlerConfig: SSRManifest,
+  _bundlerConfig: SSRModuleMap,
+  _moduleLoading: ModuleLoading,
   _callServer: CallServerCallback,
+  _nonce: ?string,
   _chunks: Map<number, SomeChunk<any>>,
   _fromJSON: (key: string, value: JSONValue) => any,
   _stringDecoder: StringDecoder,
@@ -706,13 +710,17 @@ function missingCall() {
 }
 
 export function createResponse(
-  bundlerConfig: SSRManifest,
+  bundlerConfig: SSRModuleMap,
+  moduleLoading: ModuleLoading,
   callServer: void | CallServerCallback,
+  nonce: void | string,
 ): Response {
   const chunks: Map<number, SomeChunk<any>> = new Map();
   const response: Response = {
     _bundlerConfig: bundlerConfig,
+    _moduleLoading: moduleLoading,
     _callServer: callServer !== undefined ? callServer : missingCall,
+    _nonce: nonce,
     _chunks: chunks,
     _stringDecoder: createStringDecoder(),
     _fromJSON: (null: any),
@@ -771,6 +779,12 @@ function resolveModule(
   );
   const clientReference = resolveClientReference<$FlowFixMe>(
     response._bundlerConfig,
+    clientReferenceMetadata,
+  );
+
+  prepareDestinationForModule(
+    response._moduleLoading,
+    response._nonce,
     clientReferenceMetadata,
   );
 

packages/react-client/src/forks/ReactFlightClientConfig.custom.js
@@ -25,7 +25,8 @@
 
 declare var $$$config: any;
 
-export opaque type SSRManifest = mixed;
+export opaque type ModuleLoading = mixed;
+export opaque type SSRModuleMap = mixed;
 export opaque type ServerManifest = mixed;
 export opaque type ServerReferenceId = string;
 export opaque type ClientReferenceMetadata = mixed;
@@ -35,6 +36,8 @@ export const resolveServerReference = $$$config.resolveServerReference;
 export const preloadModule = $$$config.preloadModule;
 export const requireModule = $$$config.requireModule;
 export const dispatchHint = $$$config.dispatchHint;
+export const prepareDestinationForModule =
+  $$$config.prepareDestinationForModule;
 export const usedWithSSR = true;
 
 export opaque type Source = mixed;

commit 8b6dec4cd10746e3358e9038bccd17c367916c76

[RSC-PATCH] Fix chunk skip bug when CSS files precede JS in chunk.files Set
The `for...of` loop iterating `c.files` inside a `forEach` callback
used `return` to skip non-JS files. Because `return` exits the
`forEach` callback (not the `for` loop), any chunk whose files Set
iterated a `.css` file before the `.js` file was skipped entirely —
the JS file was never discovered and the chunk was omitted from the
client manifest.

This caused RSC to silently fail to locate client components whenever
MiniCssExtractPlugin (or similar) produced `.css` chunk files whose
Set insertion order placed them before the `.js` entry.

Fix: replace `return` with `continue` so non-JS files are skipped
within the loop while the search for the `.js` file continues.

Upstream bug: facebook/react — introduced in commit 701ac2e5
(PR #27314, Sep 2023), still unfixed on `main` as of Mar 2026.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

packages/react-server-dom-webpack/src/ReactFlightWebpackPlugin.js
@@ -279,8 +279,13 @@ export default class ReactFlightWebpackPlugin {
             chunkGroup.chunks.forEach(function (c) {
               // eslint-disable-next-line no-for-of-loops/no-for-of-loops
               for (const file of c.files) {
-                if (!file.endsWith('.js')) return;
-                if (file.endsWith('.hot-update.js')) return;
+                // Note: `continue` (not `return`) is intentional here.
+                // `return` would exit the forEach callback, skipping the
+                // entire chunk when a non-JS file (e.g. .css from
+                // MiniCssExtractPlugin) appears before the .js file in
+                // the chunk's files Set.
+                if (!file.endsWith('.js')) continue;
+                if (file.endsWith('.hot-update.js')) continue;
                 chunks.push(c.id, file);
                 break;
               }